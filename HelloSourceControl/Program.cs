c11ef9d56103e1952d03f0d229e6838ae8fb184a
doms92 <summers8734@student.cptc.edu>
2021-02-11 17:01:23 -0800

    Hello World Updated

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -10,7 +10,7 @@ namespace HelloSourceControl
     {
         static void Main()
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
 
             Console.ReadKey();
         }
